8109bb8ef93d3039fec2325bf235b384d2c5d761
Jasmine Woo <jwoo@ualberta.ca>
2014-08-02 00:16:37 -0600

    Working on layouts. Added split actionbar will add navigation tabs later

--- GPACalculator/src/com/calc/gpacalculator/MainActivity.java
+++ GPACalculator/src/com/calc/gpacalculator/MainActivity.java
@@ -3,9 +3,11 @@ package com.calc.gpacalculator;
 import java.util.ArrayList;
 
 import controller.Gpa_ListAdapter;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -26,35 +28,37 @@ public class MainActivity extends ActionBarActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.task_activity);
+		setContentView(R.layout.activity_main);
 		
-		/// MOVE TO TASKACTIVITY.JAVA ///
+		// TO TEST CODE BELOW, CHANGE setContentView(R.layout.activity_main); TO setContentView(R.layout.task_activity);
 		
-		add_task = (Button) findViewById(R.id.add_taskButton);
-		task_list = (ListView) findViewById(R.id.view_tasklist);
-		
-		
-			// CREATE IN NEW SETUP ADAPTER METHOD //
-		adapter = new Gpa_ListAdapter(this, R.layout.task_entity, new ArrayList<Task>());
-		ListView activity_taskview = task_list;
-		activity_taskview.setAdapter(adapter);
-			//	END OF NEW METHOD //
-		
-			//	CREATE IN NEW SETUP BUTTON METHOD //
-		add_task.setOnClickListener(new OnClickListener() {
-			
-			public void onClick(View v) {
-				
-				adapter.insert(new Task("", 0, 0), 0);
-				
-			}
-			
-		});
-		
-			//	END OF NEW METHOD	//
-		
-		
-		//// END OF MOVE ////
+//		/// MOVE TO TASKACTIVITY.JAVA ///
+//		
+//		add_task = (Button) findViewById(R.id.add_taskButton);
+//		task_list = (ListView) findViewById(R.id.view_tasklist);
+//		
+//		
+//			// CREATE IN NEW SETUP ADAPTER METHOD //
+//		adapter = new Gpa_ListAdapter(this, R.layout.task_entity, new ArrayList<Task>());
+//		ListView activity_taskview = task_list;
+//		activity_taskview.setAdapter(adapter);
+//			//	END OF NEW METHOD //
+//		
+//			//	CREATE IN NEW SETUP BUTTON METHOD //
+//		add_task.setOnClickListener(new OnClickListener() {
+//			
+//			public void onClick(View v) {
+//				
+//				adapter.insert(new Task("", 0, 0), 0);
+//				
+//			}
+//			
+//		});
+//		
+//			//	END OF NEW METHOD	//
+//		
+//		
+//		//// END OF MOVE ////
 		
 		
 	}
@@ -62,8 +66,10 @@ public class MainActivity extends ActionBarActivity {
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+		MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.main, menu);
+
+        return super.onCreateOptionsMenu(menu);
 	}
 
 	@Override
@@ -71,10 +77,31 @@ public class MainActivity extends ActionBarActivity {
 		// Handle action bar item clicks here. The action bar will
 		// automatically handle clicks on the Home/Up button, so long
 		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
+//		int id = item.getItemId();
+//		if (id == R.id.action_settings) {
+//			return true;
+//		}
+//		return super.onOptionsItemSelected(item);
+//	}
+		switch (item.getItemId()) {
+	  
+	    case R.id.itemid_0:
+	      Toast.makeText(this, "Item1 selected", Toast.LENGTH_SHORT)
+	          .show();
+	      break;
+	    case R.id.itemid_1:
+		      Toast.makeText(this, "Item1 selected", Toast.LENGTH_SHORT)
+		          .show();
+		      break;
+	    case R.id.itemid_2:
+		      Toast.makeText(this, "Item1 selected", Toast.LENGTH_SHORT)
+		          .show();
+		      break;
+	   
+	  
+	    default:
+	      break;
+	    }
+		return true;
 	}
 }
